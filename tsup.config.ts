da2aab73eced08eea576cfe0641a4499fe4d079c
thundermiracle <cissoliu@gmail.com>
2022-03-17 22:27:01 +0900

    fix: css not bundled

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/gatsby-browser.ts'],
+  injectStyle: true,
+  platform: 'browser',
   minify: true,
   clean: true,
   sourcemap: true,
